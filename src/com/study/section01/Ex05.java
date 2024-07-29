d305980cb956ac7c45c8952b4f374e140b4fae4f
jang314 <jang314@naver.com>
2024-07-30 06:27:18 +0900

    [1장] 못푼문제 다시 혼자 풀어보기

--- src/com/study/section01/Ex05.java
+++ src/com/study/section01/Ex05.java
@@ -1,9 +1,6 @@
 package com.study.section01;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 import java.util.regex.Pattern;
 /**
  * 설명
@@ -25,28 +22,33 @@ import java.util.regex.Pattern;
 public class Ex05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-
-        Stack<Character> stack = new Stack<>();
-
-        for(int i = input.length()-1; i >= 0; i--) {
-            char c = input.charAt(i);
-            if(Pattern.matches("^[a-z]$", String.valueOf(c))) {
-                System.out.printf("push");
-                stack.push(c);
-            }
-        }
-
-        while(!stack.isEmpty()) {
-            for(int i = 0 ; i < input.length(); i++) {
-                if(!Pattern.matches("[a-z]", String.valueOf(input.charAt(i)))) {
-                    System.out.print(input.charAt(i));
-                } else {
-                    System.out.print(stack.pop());
-                }
-            }
-        }
-
-
+        String input = sc.next();
+        System.out.println(solution(input));
     }
+
+
+    private static String solution(String str) {
+        StringBuffer sb = new StringBuffer();
+        int lt = 0 ;
+        int gt = str.length() -1;
+        char[] ch = str.toCharArray();
+        while(lt < gt) {
+            if(!Character.isAlphabetic(ch[lt])) {
+                lt++;
+            } else if(!Character.isAlphabetic(ch[gt])) {
+                gt--;
+            } else {
+                char tmp = ch[lt];
+                ch[lt] = ch[gt];
+                ch[gt] = tmp;
+                lt++;
+                gt--;
+            }
+        }
+        for(char c : ch) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
 }
